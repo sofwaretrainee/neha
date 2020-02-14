2cf1de727b050ab225089f3adfc8261d49002ba4
nehak2020 <neha_k@ndvor.com>
2020-02-14 18:36:01 +0530

    added

---
+++ 14-feb-20/Student_Register/Student_Register/Register_Page.aspx
@@ -0,0 +1,103 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Register_Page.aspx.cs" Inherits="Student_Register.Register_Page" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        .container {
+            border: 1px solid black;
+            width: 350px;
+            height: 400px;
+            margin-left: 500px;
+            margin-top: 100px;
+        }
+
+        .StudentFname {
+            padding-left: 40px;
+            padding-top: 50px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container StudentFname ">
+            <asp:Label runat="server" Text="studentId"></asp:Label>
+            <asp:TextBox ID="txtstudentId" runat="server" Width="140" Style="margin-left: 19px" />
+            <div>
+                <asp:Label runat="server" Text="studentFirst_name"></asp:Label>
+                <asp:TextBox ID="txtName" runat="server" Width="140" Style="margin-left: 19px" />
+            </div>
+            <br />
+            <div>
+                <asp:Label runat="server" Text="studentLast_name"></asp:Label>
+                <asp:TextBox ID="txtLastName" runat="server" Width="140" Style="margin-left: 19px" />
+            </div>
+            <br />
+            <div>
+                <asp:Label ID="Label1" runat="server" Text="student_gender"></asp:Label>
+                <asp:RadioButtonList ID="RadioButtonList1" runat="server" Width="140" Style="margin-left: 150px">
+                    <asp:ListItem>Male</asp:ListItem>
+                    <asp:ListItem>FeMale</asp:ListItem>
+                </asp:RadioButtonList>
+            </div>
+            <br />
+            <div>
+                <asp:Label runat="server" Text="student_Dob"></asp:Label>
+                <asp:TextBox ID="txtDob" runat="server" Width="140" type="Date" Style="margin-left: 47px" />
+            </div>
+            <br />
+            <div>
+                <asp:Label runat="server" Text="Passsword"></asp:Label>
+                <asp:TextBox ID="TextPassword" runat="server" Width="140" MaxLength="10" Style="margin-left: 58px" TextMode="Password" />
+            </div>
+            <br />
+            <div>
+                <asp:Label runat="server" Text="student_mobileno"></asp:Label>
+                <asp:TextBox ID="txtMblNo" runat="server" Width="140" Style="margin-left: 16px;" MaxLength="10" />
+            </div>
+            <br />
+
+            <div>
+                <asp:Label runat="server" Text="student_address"></asp:Label>
+                <asp:TextBox ID="txtAddress" runat="server" Width="140" Style="margin-left: 26px" />
+            </div>
+            <br />
+            <div style="margin-left: 130px">
+                <asp:Button ID="ButtonSubmit" runat="server" Text="Submit" OnClick="ButtonSubmit_Click" />
+                <asp:Button ID="ButtonCancel" runat="server" Text="Cancel"  OnClick="ButtonCancel_Click" Style="margin-left: 13px" />
+                 <asp:Button ID="ButtonDelte" runat="server" Text="Delte"  OnClick="ButtonDelte_Click" Style="margin-left: 13px" />
+            </div>
+        </div>
+        <br />
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false">
+            <Columns>
+                <asp:BoundField HeaderText="studentId" DataField="studentId" />
+                <asp:BoundField HeaderText="studentFirst_name" DataField="studentFirst_name" />
+                <asp:BoundField HeaderText="studentLast_name" DataField="studentLast_name" />
+                <asp:BoundField HeaderText="student_gender" DataField="student_gender" />
+                <asp:BoundField HeaderText="student_Dob" DataField="student_Dob" />
+                <asp:BoundField HeaderText="Passsword" DataField="Passsword" />
+                <asp:BoundField HeaderText="student_mobileno" DataField="student_mobileno" />
+                 <asp:BoundField HeaderText="student_address" DataField="student_address" />
+
+
+
+            </Columns>
+        </asp:GridView>
+    </form>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 14-feb-20/Student_Register/Student_Register/Register_Page.aspx.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace Student_Register
+{
+    public partial class Register_Page : System.Web.UI.Page
+    {
+        string connstr = @"Server = NDVORSVR02\SQL2014; Initial Catalog=student_info;User ID=wmuser1;Password=wmuser1";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGrid();
+
+            }
+        }
+
+
+        public void BindGrid()
+        {
+
+            using (SqlConnection con = new SqlConnection(connstr))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetStudentDetails", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    //cmd.Parameters.Add("@studentId", SqlDbType.VarChar).Value = "Select";
+                    //cmd.Parameters.Add("@studentFirst_name", SqlDbType.VarChar).Value = "Select";
+                    con.Open();
+
+                    DataTable dt = new DataTable();
+                    using (var da = new SqlDataAdapter(cmd))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        da.Fill(dt);
+                    }
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                    con.Close();
+                }
+            }
+        }
+        protected void Insert(string studentFirst_name, string studentLast_name, string student_gender, string dateOfBirth, string Password, string student_mobileno, string student_address)
+        {
+            using (SqlConnection con = new SqlConnection(connstr))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetStudentData", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@studentId", SqlDbType.Int).Value = txtstudentId.Text;
+                    cmd.Parameters.Add("@studentFirst_name", SqlDbType.VarChar).Value = txtName.Text;
+                    cmd.Parameters.Add("@studentLast_name", SqlDbType.VarChar).Value = txtLastName.Text;
+                    cmd.Parameters.Add("@student_gender", SqlDbType.VarChar).Value = RadioButtonList1.Text;
+                    cmd.Parameters.Add("@student_Dob", SqlDbType.Date).Value = txtDob.Text;
+                    cmd.Parameters.Add("@Passsword", SqlDbType.VarChar).Value = TextPassword.Text;
+                    cmd.Parameters.Add("@student_mobileno", SqlDbType.VarChar).Value = txtMblNo.Text;
+                    cmd.Parameters.Add("@student_address", SqlDbType.VarChar).Value = txtAddress.Text;
+                    cmd.Parameters.Add("@statementType", SqlDbType.VarChar).Value = "Insert";
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    BindGrid();
+                }
+            }
+        }
+
+        protected void ButtonSubmit_Click(object sender, EventArgs e)
+        {
+            Insert(txtName.Text, txtLastName.Text, RadioButtonList1.Text, txtDob.Text, TextPassword.Text, txtMblNo.Text, txtAddress.Text);
+        }
+
+        protected void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            txtstudentId.Text = String.Empty;
+            txtName.Text = String.Empty;
+            txtLastName.Text = String.Empty;
+            RadioButtonList1.Text = String.Empty;
+            txtDob.Text = String.Empty;
+            TextPassword.Text = String.Empty;
+            txtDob.Text = String.Empty;
+            txtMblNo.Text = String.Empty;
+            txtAddress.Text = String.Empty;
+        }
+       
+
+         protected void Delete()
+        {
+            using (SqlConnection con = new SqlConnection(connstr))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetStudentData", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@statementType", "Delete");  
+                    cmd.Parameters.AddWithValue("@studentId", txtstudentId.Text);
+                    cmd.Parameters.AddWithValue("@studentFirst_name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@studentLast_name", txtLastName.Text);
+                    cmd.Parameters.AddWithValue("@student_gender", RadioButtonList1.Text);
+                    cmd.Parameters.AddWithValue("@student_Dob", txtDob.Text);
+                    cmd.Parameters.AddWithValue("@Passsword", TextPassword.Text);
+                    cmd.Parameters.AddWithValue("@student_mobileno", txtMblNo.Text);
+                    cmd.Parameters.AddWithValue("@student_address", txtAddress.Text);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    BindGrid();
+                }
+            }
+        }
+        protected void ButtonDelte_Click(object sender, EventArgs e)
+        {
+            Delete();
+        }
+    }
+
+}
+
+
